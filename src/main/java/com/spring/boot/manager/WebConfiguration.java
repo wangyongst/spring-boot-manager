04c6af53964d39e41fe2b60d5616a9a17bc6e3b4
wangyongst <wangyongst@gmail.com>
2019-06-07 00:01:58 +0800

    home

--- src/main/java/com/spring/boot/manager/WebConfiguration.java
+++ src/main/java/com/spring/boot/manager/WebConfiguration.java
@@ -1,9 +1,6 @@
 package com.spring.boot.manager;
 
 import com.spring.boot.manager.config.LogInterceptor;
-import com.spring.boot.manager.service.impl.UserServiceImpl;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
