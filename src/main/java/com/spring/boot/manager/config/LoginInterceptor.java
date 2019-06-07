dacc835deae402ccddcca03b123f66b79e20e06f
wangyongst <wangyongst@gmail.com>
2019-06-07 22:51:39 +0800

    home

---
+++ src/main/java/com/spring/boot/manager/config/LoginInterceptor.java
@@ -0,0 +1,38 @@
+package com.spring.boot.manager.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@Component
+public class LoginInterceptor implements HandlerInterceptor {
+
+    private static Logger logger = LoggerFactory.getLogger(LoginInterceptor.class);
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        return true;
+    }
+
+    private void returnJson(HttpServletResponse response, String json) throws Exception{
+        PrintWriter writer = null;
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html; charset=utf-8");
+        try {
+            writer = response.getWriter();
+            writer.print(json);
+
+        } catch (IOException e) {
+            logger.error("response error",e);
+        } finally {
+            if (writer != null)
+                writer.close();
+        }
+    }
+}
