d64be9b7634ba17bbab2fa362b1f117bb2d7b739
wangyongst <wangyongst@gmail.com>
2019-06-18 13:49:17 +0800

    office

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -8,6 +8,14 @@ $(function () {
         $('#projectModal').modal('toggle');
     });
 
+    $("#resourcenewButton").click(function () {
+        $('#resourceModal').modal('toggle');
+    });
+
+    $("#suppliernewButton").click(function () {
+        $('#supplierModal').modal('toggle');
+    });
+
     $("#projectsaveButton").click(function () {
         $.post("admin/project/sud", $('#projectForm').serialize(),
             function (result) {
@@ -37,7 +45,31 @@ $(function () {
                     $('#project-list-table').bootstrapTable("destroy");
                     $('#project-list-table').bootstrapTable({url: "/admin/project/list?" + $('#searchprojectForm').serialize()});
                 });
+        } else if (deletetype == 2) {
+            $.post("admin/resource/sud",
+                {
+                    resourceid: deleteid,
+                    delete: 1,
+                },
+                function (result) {
+                    $('#deletealertModal').modal('toggle');
+                    $('#resource-list-table').bootstrapTable("destroy");
+                    $('#resource-list-table').bootstrapTable({url: "/admin/source/list?" + $('#searchresourceForm').serialize()});
+                });
+        } else if (deletetype == 3) {
+            $.post("admin/supplier/sud",
+                {
+                    supplierid: deleteid,
+                    delete: 1,
+                },
+                function (result) {
+                    $('#deletealertModal').modal('toggle');
+                    $('#supplier-list-table').bootstrapTable("destroy");
+                    $('#supplier-list-table').bootstrapTable({url: "/admin/supplier/list?" + $('#searchsupplierForm').serialize()});
+                });
         }
+
+
     });
 
 
@@ -51,18 +83,41 @@ $(function () {
         $('#supplier-list-table').bootstrapTable({url: "/admin/supplier/list?" + $('#searchsupplierForm').serialize()});
     });
 })
-;
+
 
 function updateproject(value) {
     window.location.href = "user-update.html?" + value;
-};
+}
 
 function delproject(value) {
     $('#deletetype').val(1);
     $('#deletevalue').val(value);
     $('#deletealertmessage').text("确定要删除这个项目吗？");
     $('#deletealertModal').modal('toggle');
-};
+}
+
+function updateresource(value) {
+    window.location.href = "user-update.html?" + value;
+}
+
+function delresource(value) {
+    $('#deletetype').val(2);
+    $('#deletevalue').val(value);
+    $('#deletealertmessage').text("确定要删除这个资源配置吗？");
+    $('#deletealertModal').modal('toggle');
+}
+
+
+function updatesupplier(value) {
+    window.location.href = "user-update.html?" + value;
+}
+
+function delsupplier(value) {
+    $('#deletetype').val(3);
+    $('#deletevalue').val(value);
+    $('#deletealertmessage').text("确定要删除这个供应商吗？");
+    $('#deletealertModal').modal('toggle');
+}
 
 
 function projectformatter(value, row, index) {
