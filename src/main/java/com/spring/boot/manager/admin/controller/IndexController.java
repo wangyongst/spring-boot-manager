14c64a038f893031d62d0edfa43898c0f2676b2d
wangyongst <wangyongst@gmail.com>
2019-06-28 16:36:56 +0800

    office

--- src/main/java/com/spring/boot/manager/admin/controller/IndexController.java
+++ src/main/java/com/spring/boot/manager/admin/controller/IndexController.java
@@ -1,13 +1,28 @@
 package com.spring.boot.manager.admin.controller;
 
+import com.spring.boot.manager.model.AdminParameter;
+import com.spring.boot.manager.service.AdminService;
+import com.spring.boot.manager.service.AdminTwoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
+    @Autowired
+    public AdminService adminService;
+
+    @Autowired
+    public AdminTwoService adminTwoService;
+
     @RequestMapping("/")
-    public String index() {
+    public String index(Model model) {
+        AdminParameter adminParameter = new AdminParameter();
+        model.addAttribute("resources", adminTwoService.resourceList(adminParameter).getData());
+        model.addAttribute("projects", adminTwoService.projectList(adminParameter).getData());
+        model.addAttribute("suppliers", adminTwoService.supplierList(adminParameter).getData());
         return "index";
     }
 
