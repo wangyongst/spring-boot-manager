dac20abf30d0e7f21e677fd1a420c6a9afb44f8a
wangyongst <wangyongst@gmail.com>
2019-08-28 12:36:30 +0800

    office

--- src/main/java/com/spring/boot/manager/entity/Request.java
+++ src/main/java/com/spring/boot/manager/entity/Request.java
@@ -50,6 +50,18 @@ public class Request {
     @Column(name = "createtime", nullable = true, length = 255)
     private String createtime;
 
+    @Basic
+    @Column(name = "status", nullable = true)
+    private Integer status;
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
     public BigDecimal getPrice2() {
         return price2;
     }
