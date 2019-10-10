7ffc11d80d1139c2b9fc1f57a8c05f580bf83be4
wangyongst <wangyongst@gmail.com>
2019-10-10 15:34:27 +0800

    office

--- src/main/resources/static/js/request-list.js
+++ src/main/resources/static/js/request-list.js
@@ -223,7 +223,7 @@ function statusformatter(value, row, index) {
     else if (value == 2 && row["type"] != 1) return "待审核";
     else if (value == 3) return "待接单";
     else if (value == 4) return "下单失败";
-    else if (value == 0) return "已取消";
+    else if (value == 0) return "取消订单";
     else if (value == 5) return "生产中";
     else if (value == 6) return "送货中";
     else if (value == 7) return "已完成";
